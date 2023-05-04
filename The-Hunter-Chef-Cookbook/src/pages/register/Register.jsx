96fccd21b3d16d9ec88c964fe94047eb78bc468b
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-05-04 21:47:17 +0600

    email password authentication done

--- The-Hunter-Chef-Cookbook/src/pages/register/Register.jsx
+++ The-Hunter-Chef-Cookbook/src/pages/register/Register.jsx
@@ -5,17 +5,25 @@ import { useState } from 'react';
 const Register = (event) => {
     const [email, setEmail] = useState('');
     // console.log(event.target);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const name = event.target.name.value;
+        console.log(email, password, name);
+    }
     
-    
-    const handleEmailChange = () => {
+    const handleEmailChange = (event) => {
+        console.log(event.target.value);
+        setEmail(event.target.value);
 
     }
     return (
         <div>
-            <form>
-                <input onChange={handleEmailChange} type="email" name="name" id="" placeholder='Your Name'/>
+            <form onSubmit={handleSubmit}>
+                <input type="name" name="name" id="" placeholder='Your Name'/>
                 <br></br>
-                <input type="email" name="email" id="email" placeholder='Your Email'/>
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email'/>
                 <br></br>
                 <input type="password" name="password" id="" placeholder='Your password'/>
                 <input type="submit" value="register" />
