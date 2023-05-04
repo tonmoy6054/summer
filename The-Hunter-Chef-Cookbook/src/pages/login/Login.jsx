b8692781faa36b4e223db6b134b735c904dafe17
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-05-04 21:15:22 +0600

    github sign in done

--- The-Hunter-Chef-Cookbook/src/pages/login/Login.jsx
+++ The-Hunter-Chef-Cookbook/src/pages/login/Login.jsx
@@ -1,28 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { FaGithub,FaGoogle } from 'react-icons/fa';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
+import {GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
 import app from '../../firebase/Firebase.config';
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
         .then(result => {
-            const user = result.user;
-            console.log(user);
+            const loggedInUser = result.user;
+            setUser(loggedInUser);
         })
         .catch(error => {
             console.log('error', error.message);
         })
-       
     }
+
+
+        const handleGithubSignIn = () => {
+            signInWithPopup(auth, githubProvider)
+            .then(result => {
+const loggedUser = result.user;
+setUser(loggedUser);
+            })
+            .catch(error => {
+console.log(error);
+            })
+
+        }
+       
+    
     return (
         <div>
             <h4>login with</h4>
+            
             <Button variant="outline-primary" onClick={handleGoogleSignIn}className="mb-2"><FaGoogle /> login with Google</Button>
-      <Button variant="outline-secondary"><FaGithub />login with Github</Button>
+
+      <Button variant="outline-secondary" onClick={handleGithubSignIn}><FaGithub />login with Github</Button>
+      
         </div>
     );
 };
